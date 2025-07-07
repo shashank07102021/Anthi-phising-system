524b6b32ca9937b87034a2cb00b7a6c5f35cb32c
shashank07102021 <shashankvenkatesh270@gmail.com>
2025-07-07 22:03:26 +0530

    Day _4

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask,render_template,request
 from url_checker import check_url
+from flask import request
+import csv
+from datetime import datetime
 
 app=Flask(__name__)
+def log_scan(url, result, ip_address, user_agent):
+    with open("scan_log.csv", "a", newline='', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), url, result, ip_address, user_agent])
+                        
+ 
 
 @app.route('/' ,methods=['GET','POST'])
 def home():
@@ -10,7 +19,19 @@ def home():
         url=request.form['url']
         result=check_url(url)
 
+        ip_address  =request.remote_addr
+        user_agent = request.headers.get ('usre-agent')   
+        log_scan(check_url, result, ip_address, user_agent)
+
+    # ✅ HERE is Step 4 — show result page
+        return render_template("result.html", url=check_url, result=result)
+
     return render_template('index.html',result=result)
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
+
